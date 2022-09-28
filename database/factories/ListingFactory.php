787e53b7dc8ee088150f8603ff72a2ec99dfce43
ixmprove <thijsboelman01@gmail.com>
2022-09-28 19:45:43 +0200

    Added pagination // updated Factory to Randomize Tags

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -18,12 +18,58 @@ class ListingFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence(),
-            'tags' => 'laravel, api, backend',
+            'tags' => $this->getRandomTags(rand(2, 4)),
             'company' => $this->faker->company(),
             'location' => $this->faker->city(),
             'email' => $this->faker->companyEmail(),
             'website' => $this->faker->url(),
             'description' => $this->faker->paragraph(5)
         ];
+    }
+
+    private function getRandomTags(int $amount)
+    {
+        $tag = [
+            'javascript',
+            'api',
+            'backend',
+            'frontend',
+            'front-end',
+            'back-end',
+            'linux',
+            'devops',
+            'php',
+            'html',
+            'css',
+            'react',
+            'vue',
+            'angular',
+            'Java',
+            'C++',
+            'c#',
+            'Python',
+            'MongoDB',
+            'MySQL',
+            'SQL',
+            'Express',
+            'Node.js',
+            'Docker',
+            'Bash',
+        ];
+
+        $tags = [];
+
+        for ($i = 0; $i < $amount; $i++) {
+            $randomTag = $tag[rand(0, count($tag) - 1)];
+
+            if (!in_array($randomTag, $tags)) {
+                array_push($tags, $randomTag);
+                continue;
+            } else {
+                $i--;
+            }
+        };
+
+        return implode(',', $tags);
     }
 }
