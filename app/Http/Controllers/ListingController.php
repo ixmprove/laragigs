2f08f8d2330445578ceb5e7cf00d26784c14476a
ixmprove <thijsboelman01@gmail.com>
2022-09-28 18:49:57 +0200

    flash message on Listing POST

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -56,6 +56,6 @@ class ListingController extends Controller
 
         Listing::create($formFields);
 
-        return redirect('/');
+        return redirect('/')->with('message', 'Listing Created Successfully');
     }
 }
