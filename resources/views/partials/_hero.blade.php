9a394ac2b49c81c6b331e6d493235ccdb1abcd11
ixmprove <thijsboelman01@gmail.com>
2022-09-29 04:22:34 +0200

    Fixed urls

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -8,7 +8,11 @@
                     Find or post Laravel jobs & projects
         </p>
         <div>
-            <a href="/register" class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign Up to List a Gig</a>
+            @auth
+                <a href="/listings/create" class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Post Your Job</a>
+            @else
+                <a href="/register" class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign Up to List a Gig</a>
+            @endauth
         </div>
     </div>
 </section>
